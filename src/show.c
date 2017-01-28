686e9ef3223428f3b2975c3abe33c6ea099bc336
David Zeng <david.zeng@epitech.eu>
2017-01-28 16:17:04 +0100

    delete if in show()

--- src/show.c
+++ src/show.c
@@ -16,17 +16,14 @@ void		show_alloc_mem()
   t_node	*cur_ptr;
 
   printf("break : %p\n", sbrk(0));
-  if (head)
+  cur_ptr = head;
+  while (cur_ptr && cur_ptr != get_block_at(last, last->size))
     {
-      cur_ptr = head;
-      while (cur_ptr != get_block_at(last, last->size))
-	{
-	  if (!cur_ptr->is_free)
-	    printf("%p - %p : %lu bytes\n",
-		   cur_ptr + 1,
-		   get_block_at(cur_ptr, cur_ptr->size),
-		   cur_ptr->size);
-	  cur_ptr = get_block_at(cur_ptr, cur_ptr->size);
-	}
+      if (!cur_ptr->is_free)
+	printf("%p - %p : %lu bytes\n",
+	       cur_ptr + 1,
+	       get_block_at(cur_ptr, cur_ptr->size),
+	       cur_ptr->size);
+      cur_ptr = get_block_at(cur_ptr, cur_ptr->size);
     }
 }
